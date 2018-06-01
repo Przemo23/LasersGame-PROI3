128bd3bf86a87ea0c6ce4c91bc3096a02efdc932
Przemo23 <37190214+Przemo23@users.noreply.github.com>
2018-06-01 17:24:02 +0200

    Add files via upload
    Wczesna wersja z pryzmatem. Wykonane:
    - Wykrywanie kolizji z pryzmatem
    - Dodawanie pryzmatu
    - Zakrzywianie toru lotu(będą potrzebne małe poprawki)
    Do zrobienia:
    - Rozszczepienie światła
    - Obrót pryzmatu

---
+++ Prism.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include <cmath>
+#include <SFML\Graphics.hpp>
+#include "Prism.h"
+#include "Sat.h"
+#include "SourceLaser.h"
+#define PI 3.14159265
+
+
+using namespace sf;
+
+Prism::Prism(int x, int y)
+{
+	CoordinatesX = x;
+	CoordinatesY = y;
+	PrismShape.setSize(Vector2f(20.0f, 20.0f));
+	PrismShape.setPosition(x*30.0 + 16.0, y*30.0 + 16.0);
+	PrismShape.setOrigin(Vector2f(10.0f, 10.0f));
+	PrismShape.setFillColor(Color::Magenta);
+	
+	
+	RefractionRate = 1.5;
+	
+
+}
+
+void Prism::Refraction(sf::RectangleShape Laser, sf::Vector2f* Movement)
+{
+	
+	
+		Vector2f TempDirection, PrismSize;
+		float IncidenceAngle, ReflectionAngle;
+		float TemporaryValue;
+		PrismSize = PrismShape.getSize();
+		
+		
+		TempDirection.x = (Movement->x * cos(2 * PI - Rotation * PI / 180) - Movement->y * sin(2 * PI - Rotation * PI / 180));
+		TempDirection.y = Movement->x * sin(2 * PI - Rotation * PI / 180) + Movement->y * cos(2 * PI - Rotation * PI / 180);
+		TemporaryValue = sqrt(TempDirection.x*TempDirection.x + TempDirection.y*TempDirection.y);
+		IncidenceAngle = abs(asin(sin(TempDirection.y / TemporaryValue)));
+		ReflectionAngle = asin(sin(IncidenceAngle) / RefractionRate);
+		TempDirection.x = TempDirection.x * cos(IncidenceAngle - ReflectionAngle) - TempDirection.y * sin(IncidenceAngle - ReflectionAngle);
+		TempDirection.y = TempDirection.x * sin(IncidenceAngle - ReflectionAngle) + TempDirection.y * cos(IncidenceAngle - ReflectionAngle);
+		Movement->x = TempDirection.x * cos(Rotation * PI / 180) - TempDirection.y * sin(Rotation * PI / 180);
+		Movement->y = TempDirection.x * sin(Rotation * PI / 180) + TempDirection.y * cos(Rotation * PI / 180);
+	
+
+
+
+}
